5e8849679eb13b8b11b39f2aeb76e4bc6091826f
LoganTorres <logant2@uci.edu>
2025-02-05 23:59:05 -0800

    Comments and planning for tomorrow

--- crawler/worker.py
+++ crawler/worker.py
@@ -8,7 +8,16 @@ import time
 
 
 class Worker(Thread):
-    def __init__(self, worker_id, config, frontier):
+    def __init__(self, worker_id, config, frontier): # GOOD
+        """
+        Initializes the crawler worker 
+
+        @Parameters:
+        worker_id - number to uniquely identify the crawler worker
+        config    - used for the download function
+        frontier  - used to maintain URLs that still need to be traversed
+        """
+
         self.logger = get_logger(f"Worker-{worker_id}", "Worker")
         self.config = config
         self.frontier = frontier
@@ -16,19 +25,25 @@ class Worker(Thread):
         assert {getsource(scraper).find(req) for req in {"from requests import", "import requests"}} == {-1}, "Do not use requests in scraper.py"
         assert {getsource(scraper).find(req) for req in {"from urllib.request import", "import urllib.request"}} == {-1}, "Do not use urllib.request in scraper.py"
         super().__init__(daemon=True)
-        
-    def run(self):
+    
+    def run(self): # [WIP]
+        """
+        Main crawler program where it keeps traversing the web using the frontier (which has an intial set of URLS) and
+        only stops once its empty
+        """
+
         while True:
-            tbd_url = self.frontier.get_tbd_url()
+            tbd_url = self.frontier.get_tbd_url() # Same as "popping" from queue
             if not tbd_url:
                 self.logger.info("Frontier is empty. Stopping Crawler.")
                 break
-            resp = download(tbd_url, self.config, self.logger)
+            # TODO: Check if the URL is not duplicate and main tain depth to avoid traps!
+            resp = download(tbd_url, self.config, self.logger) # Requests for page/resource and will download it (which in the real-world would be saved in the document store)
             self.logger.info(
                 f"Downloaded {tbd_url}, status <{resp.status}>, "
                 f"using cache {self.config.cache_server}.")
-            scraped_urls = scraper.scraper(tbd_url, resp)
-            for scraped_url in scraped_urls:
+            scraped_urls = scraper.scraper(tbd_url, resp) # Will get net URLs to crawl (this should also document content)
+            for scraped_url in scraped_urls: # adding URLs to the frontier
                 self.frontier.add_url(scraped_url)
             self.frontier.mark_url_complete(tbd_url)
-            time.sleep(self.config.time_delay)
+            time.sleep(self.config.time_delay) # Delay for politeness (should be set accordingly in "config.ini")
